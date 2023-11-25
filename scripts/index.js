c57b09c73c1112ad0396ada3b196410a0c4e7cbb
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-11-25 13:28:04 +0100

    main page upgrade

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+let newSong = new Audio("./sounds/mixkit-auditorium-moderate-applause-and-cheering-502.wav")
+document.getElementById('loadMe').addEventListener('load', ()=>{
+})
+
+const pauseGame = () => {
+  newSong.play()
+
+ document.getElement
+}
