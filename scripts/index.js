8b72b566e5c98d7408897d9679c8e89de2f5486b
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-12-04 20:37:39 +0100

    answer bug fixed

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ let newSong = new Audio(
 // }, 4000);
 const audio = document.getElementById("myAudio");
 // Set the volume to a lower value
-audio.volume = 0.5;
+audio.volume = 0.8;
 // You can also change the volume dynamically
 function setVolume(newVolume) {
   if (newVolume >= 0 && newVolume <= 1) {
@@ -16,7 +16,7 @@ function setVolume(newVolume) {
   }
 }
 
-setVolume(0.1);
+setVolume(0.3);
 
 
 let time;
@@ -43,14 +43,15 @@ const pauseTimer = () => {
   clearInterval(timerInterval);
 };
 
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
+ // Copy the array to avoid modifying the original
+ const shuffleArray = (array) => {
+  const shuffledArray = array.slice(); // Copy the array to avoid modifying the original
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
   }
-  return array;
+  return shuffledArray;
 };
-
 
 const fetchNewQuestion = () => {
   fetch("https://opentdb.com/api.php?amount=1&type=multiple")
@@ -59,7 +60,7 @@ const fetchNewQuestion = () => {
       let quizQuest = result.results[0];
       let answerOptions = [
         quizQuest.correct_answer,
-        quizQuest.incorrect_answers,
+        ...quizQuest.incorrect_answers,
       ];
 
       console.log("Shuffled options:", answerOptions);
@@ -78,40 +79,45 @@ const fetchNewQuestion = () => {
       questions.innerHTML =`<h5 class="questions">${cleanQuestion}</h5>`;
 
       answerQuiz.innerHTML = `
-        <div class="brder2 px-3 py-2 text-white fw-bold">A. ${answerOptions[0]}</div>
-        <div class="brder2 px-3 py-2 text-white fw-bold">B. ${answerOptions[1]}</div>
+        <div class="brder2 px-3 py-2 text-white fw-bold"> ${answerOptions[0]}</div>
+        <div class="brder2 px-3 py-2 text-white fw-bold"> ${answerOptions[1]}</div>
       `;
 
       answerQuiz1.innerHTML = `
-        <div class="brder2 px-3 py-2 text-white fw-bold">C. ${answerOptions[2]}</div>
-        <div class="brder2 px-3 py-2 text-white fw-bold">D. ${answerOptions[3]}</div>
+        <div class="brder2 px-3 py-2 text-white fw-bold"> ${answerOptions[2]}</div>
+        <div class="brder2 px-3 py-2 text-white fw-bold"> ${answerOptions[3]}</div>
       `;
 
       document.querySelectorAll(".brder2").forEach((option) => {
         option.addEventListener("click", (event) => {
           const clickedAnswer = event.target;
-          const answerText = clickedAnswer.textContent;
+          const answerText = clickedAnswer.innerText;
+          const correctAnswer = quizQuest.correct_answer
+          pauseTimer();
+          // console.log("Clicked Answer:", answerText);
+          // console.log("Correct Answer:", correctAnswer);
+      
 
-          pauseTimer()
 
-          if (answerText === quizQuest.correct_answer) {
+          if (answerText === correctAnswer) {
             clickedAnswer.style.backgroundColor = "green";
             updateReward(200);
           } else {
             clickedAnswer.style.backgroundColor = "red";
             updateReward(-30);
           }
-          
-
+      
           document.querySelectorAll(".brder2").forEach((answer) => {
             answer.style.pointerEvents = "none";
           });
-
+      
           setTimeout(() => {
             fetchNewQuestion();
           }, 2500);
         });
       });
+      
+      
 
       time = 30;
       timerInterval = setInterval(updateTimer, 1000);
@@ -120,13 +126,25 @@ const fetchNewQuestion = () => {
 
 let reward = 0;
 
-// ... (previous code remains unchanged)
+let correctAnswersCount = 0;
 
+// Update reward and check for correct answers
 const updateReward = (amount) => {
   reward += amount;
 
   const rewardDiv = document.getElementById("reward");
-  rewardDiv.innerHTML = `<h4>${reward}</h4>`;
+  rewardDiv.innerHTML = `<h4>$${reward}</h4>`;
+
+  if (amount === 200) {
+    correctAnswersCount++; // Increment count for correct answers
+  }
+
+  if (correctAnswersCount === 3) {
+    // Display congratulations image
+    document.body.innerHTML = `<img src="congratulations_image.jpg" alt="Congratulations" style="width: 100%; height: 100%; object-fit: cover;">`;
+    setVolume(0.6);
+
+  }
 
   if (reward < 100) {
     rewardDiv.style.backgroundColor = "red";
@@ -137,7 +155,6 @@ const updateReward = (amount) => {
   }
 };
 
-// ... (remaining code remains the same)
-
+// ... (other code remains the same)
 
 fetchNewQuestion();
